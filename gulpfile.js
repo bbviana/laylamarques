bae861d84909e09e475e54c49b74119d0cff6e71
bbviana <bbviana@touchhealth.com.br>
2015-09-19 20:03:48 -0300

    migrando para ec6 classes
    Change-Id: I2613eab609048ae301c6438a9bd0a2c93f4ef826

--- gulpfile.js
+++ gulpfile.js
@@ -2,26 +2,38 @@ var babelify = require("babelify");
 var browserify = require('browserify');
 var gulp = require('gulp');
 var gutil = require('gulp-util');
+var livereload = require('gulp-livereload');
 var notifier = require("node-notifier");
 var source = require('vinyl-source-stream');
 var watchify = require('watchify');
 
 var paths = {
     src: './src/main/webapp/node_modules',
-    target: './target/laylamarques/js'
+    target: './src/main/webapp/js'
+    // target: './target/laylamarques/js'
 };
 
-gulp.task('browserify', function () {
+var build = function (watch) {
     var bundler = browserify({
-        entries: [paths.src + '/main.js'], // Only need initial file, browserify finds the deps
-        transform: [babelify], /// es6 to es5 + reactify
-        debug: true, // Gives us sourcemapping
-        cache: {}, packageCache: {}, fullPaths: true // Requirement of watchify
+        entries: [paths.src + '/main.jsx'],
+        extensions: ['.js', '.jsx'],
+        transform: [
+            // es6 to es5 + reactify
+            babelify.configure({
+                stage: 0,
+                optional: ["es7.decorators"]
+            }),
+        ],
+        debug: true, // sourcemapping: TRUE em dev
+        // cache, packageCache, fullPaths: necessários pra o watchify
+        cache: {},
+        packageCache: {},
+        fullPaths: true
     });
-    var watcher = watchify(bundler);
+    //bundler.transform({global: true}, 'uglifyify');
 
     var bundle = function () {
-        watcher.bundle()
+        bundler.bundle()
             .on('error', function (error) {
                 gutil.log(gutil.colors.red(error.message));
                 notifier.notify({
@@ -31,20 +43,29 @@ gulp.task('browserify', function () {
             })
             .pipe(source('bundle.js'))
             .pipe(gulp.dest(paths.target))
+            .pipe(livereload());
     };
 
-    watcher.on('time', function (time) {
-        gutil.log('Bundle Gerado', gutil.colors.magenta(time + 'ms'));
-        notifier.notify({
-            title: 'Bundle Gerado',
-            message: time + 'ms'
-        });
-    });
+    if (watch) {
+        livereload.listen();
 
-    // When any files update
-    watcher.on('update', bundle);
+        bundler = watchify(bundler);
+
+        bundler
+            .on('update', bundle) // When any files update
+            .on('time', function (time) {
+                gutil.log('Bundle Gerado', gutil.colors.magenta(time + 'ms'));
+                notifier.notify({
+                    title: 'Bundle Gerado',
+                    message: time + 'ms'
+                });
+            });
+    }
 
     return bundle();
-});
+};
 
-gulp.task('default', ['browserify']);
+gulp.task('browserify-nowatch', build.bind(this, false));
+gulp.task('browserify-watch', build.bind(this, true));
+gulp.task('watch', ['browserify-watch']);
+gulp.task('default', ['browserify-nowatch']);
